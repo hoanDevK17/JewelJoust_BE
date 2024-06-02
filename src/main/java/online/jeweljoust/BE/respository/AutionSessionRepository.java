58654a498f0d5f63c471a7fc8ab61e66c8026b07
hoanDevK17 <hoannse170243@fpt.edu.vn>
2024-06-03 02:32:27 +0700

    Create API createAuctionSession by manager

---
+++ src/main/java/online/jeweljoust/BE/respository/AutionSessionRepository.java
@@ -0,0 +1,8 @@
+package online.jeweljoust.BE.respository;
+
+import online.jeweljoust.BE.entity.AutionSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutionSessionRepository  extends JpaRepository<AutionSession, Long> {
+
+}
